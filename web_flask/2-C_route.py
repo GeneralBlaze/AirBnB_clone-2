ad0f4d92e1034d846f0d558b78f4e3b334a6c97a
GeneralBlaze <benjaminprincewill@gmail.com>
2023-05-27 21:00:11 +0000

    2 done

--- web_flask/2-C_route.py
+++ web_flask/2-C_route.py
@@ -14,6 +14,11 @@ def hbnb():
     """ displays text on browser"""
     return 'Hello HBNB!'
 
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = escape(text.replace('_', ' '))
+    return 'C {}'.format(text)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
